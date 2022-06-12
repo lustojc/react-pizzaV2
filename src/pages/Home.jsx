88533776295ae55eed24791532a44bec42a1aa4d
lustojc <sashalusto228@gmail.com>
2022-06-12 12:20:46 +0300

    Added adaptive and cart page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,19 +18,22 @@ function Home() {
         setPizzas(data);
         setIsLoading(false);
       });
+    window.scrollTo(0, 0);
   }, []);
 
   return (
-    <div className="container">
-      <div className="content__top">
-        <Categories />
-        <Sort />
-      </div>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(8)].map((_, i) => <Skeleton key={i} />)
-          : pizzas.map((el) => <PizzaBlock key={el.id} {...el} />)}
+    <div className="content">
+      <div className="container">
+        <div className="content__top">
+          <Categories />
+          <Sort />
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">
+          {isLoading
+            ? [...new Array(8)].map((_, i) => <Skeleton key={i} />)
+            : pizzas.map((el) => <PizzaBlock key={el.id} {...el} />)}
+        </div>
       </div>
     </div>
   );
